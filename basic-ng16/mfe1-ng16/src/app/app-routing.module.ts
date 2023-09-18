bc7af8647363143d8c57f6f6727bf53606ecae4b
Eduardo Serrano <eduardo.serrano@lexisnexis.co.uk>
2023-09-18 23:53:04 +0100

    update feature module import on basic example

--- basic-ng16/mfe1-ng16/src/app/app-routing.module.ts
+++ basic-ng16/mfe1-ng16/src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MyFeatureModule } from 'src/app/my-feature/my-feature.module';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), MyFeatureModule],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
