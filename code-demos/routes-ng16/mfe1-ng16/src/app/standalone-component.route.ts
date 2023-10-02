eab0bf4a02588d631746e59499f61441ae79091e
Eduardo Serrano <eduardo.serrano@lexisnexis.co.uk>
2023-10-02 21:01:33 +0100

    Add code comment

--- code-demos/routes-ng16/mfe1-ng16/src/app/standalone-component.route.ts
+++ code-demos/routes-ng16/mfe1-ng16/src/app/standalone-component.route.ts
@@ -2,6 +2,8 @@ import { Route } from '@angular/router';
 import { MyStandaloneComponent } from './my-standalone-component/my-standalone-component.component';
 import { AnotherStandaloneComponent } from './another-standalone-component/another-standalone-component.component';
 
+// The webpack configuration file at /routes-ng16/mfe1-ng16/webpack.config.js
+// exposes a webpack module which contains this Angular routes array
 export const STANDALONE_COMPONENTS_ROUTES: Route[] = [
   {
     path: 'my',
