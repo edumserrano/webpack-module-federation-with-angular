ebda7d50ebbb8f575ee50f832b8e2477b5db9bb0
Eduardo Serrano <eduardo.serrano@lexisnexis.co.uk>
2023-10-03 18:35:14 +0100

    move code comment to correct place

--- code-demos/web-component-directive-ng16/mfe1-ng16/src/app/my-standalone-component/remote-bootstrap.ts
+++ code-demos/web-component-directive-ng16/mfe1-ng16/src/app/my-standalone-component/remote-bootstrap.ts
@@ -1,3 +1,6 @@
 import { bootstrapMyComponentAsync } from './my-standalone-component-bootstrap';
 
+// The webpack configuration file at /web-component-directive-ng16/mfe1-ng16/webpack.config.js
+// exposes a webpack module which contains this file. So when the webpack module is loaded,
+// this function is executed.
 await bootstrapMyComponentAsync();
