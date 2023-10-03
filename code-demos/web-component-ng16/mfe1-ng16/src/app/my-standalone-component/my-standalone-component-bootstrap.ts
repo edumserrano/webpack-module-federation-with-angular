76a4b96ca087a051cf9a8a1cc71114f5f10d2d4a
Eduardo Serrano <eduardo.serrano@lexisnexis.co.uk>
2023-10-03 09:42:05 +0100

    improve bootstrap function for web component

--- code-demos/web-component-ng16/mfe1-ng16/src/app/my-standalone-component/my-standalone-component-bootstrap.ts
+++ code-demos/web-component-ng16/mfe1-ng16/src/app/my-standalone-component/my-standalone-component-bootstrap.ts
@@ -8,13 +8,21 @@ import { ApplicationConfig, ApplicationRef } from '@angular/core';
 export async function bootstrapMyComponentAsync(): Promise<void> {
   // This converts the Angular component to a Web component.
   //
-  // First we instantiate an Angular application so that we can get
-  // a reference to its injector and pass it to the createCustomElement
-  // which converts the Angular component to a Web component.
+  // First we check if the custom element has already been defined. If so we don't do anything
+  // because the same custom element cannot be defined more than once.
   //
-  // Lastly, we add the Web component to the CustomElementRegistry
-  // so that when an element <my-mfe-element></my-mfe-element> exists
-  // on the html, the Web component will be rendered on it.
+  // If the custom element has not been defined, we instantiate an Angular application so that
+  // we can get a reference to its injector and pass it to the createCustomElement which converts
+  // the Angular component to a Web component.
+  //
+  // Lastly, we add the Web component to the CustomElementRegistry so that when an element
+  // <my-mfe-element></my-mfe-element> is defined on the html, the Web component will be rendered on it.
+
+  const customElementCtor: CustomElementConstructor | undefined = customElements.get('my-mfe-element');
+  if(customElementCtor) {
+    // if this custom element has already been added to the custom elements registry then do nothing.
+    return;
+  }
 
   const appConfig: ApplicationConfig = {
     providers: [], // add any required providers here
