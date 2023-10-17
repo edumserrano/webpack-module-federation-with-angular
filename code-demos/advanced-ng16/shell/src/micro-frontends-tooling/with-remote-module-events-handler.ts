0794a51488abacfb12d44c731e47768200d16e07
Eduardo Serrano <eduardo.serrano@lexisnexis.co.uk>
2023-10-17 20:14:52 +0100

    remove code comment

--- code-demos/advanced-ng16/shell/src/micro-frontends-tooling/with-remote-module-events-handler.ts
+++ code-demos/advanced-ng16/shell/src/micro-frontends-tooling/with-remote-module-events-handler.ts
@@ -8,11 +8,6 @@ import {
 import { RemoteModuleEvent } from './remote-module-events';
 import { REMOTE_MODULE_EVENTS } from './remote-module.service';
 
-// based on the withNavigationErrorHandler Angular approach
-//
-// https://github.com/angular/angular/blob/c2b1a242e8db0ef8e03f7ee85ffa1f82562fd735/packages/router/src/provide_router.ts#L637-L652
-// https://medium.com/@artur.fedotiew/%EF%B8%8F-simplifying-navigation-error-handling-with-angulars-upcoming-feature-%EF%B8%8F-b55ee04d246a
-
 export function withRemoteModuleEventsHandler(
   fn: (event: RemoteModuleEvent) => void
 ): Provider {
