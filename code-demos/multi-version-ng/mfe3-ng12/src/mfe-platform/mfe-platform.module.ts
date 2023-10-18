0cdcf59bf94f89aa9de7ea05906d1cc6c5f1095c
Eduardo Serrano <eduardo.serrano@lexisnexis.co.uk>
2023-10-18 01:36:02 +0100

    Update code TODO

--- code-demos/multi-version-ng/mfe3-ng12/src/mfe-platform/mfe-platform.module.ts
+++ code-demos/multi-version-ng/mfe3-ng12/src/mfe-platform/mfe-platform.module.ts
@@ -16,6 +16,9 @@ export class MfePlatformModule {
 
   public constructor(private readonly _injector: Injector) {}
 
+  // TODO explain that this ngDoBootstrap method is called in the absence of elements in the bootstrap array
+  // (or review in docs the condition that makes it execute) and is used to bootstrap
+  // the mfe platform module
   // TODO can I customize the element name somehow? test again passing some providers when bootstrapping the app
   public ngDoBootstrap() : void {
     const customElementName = "mfe3-element";
