83d7a7e921a06ece155aa11666b091c3481e759a
Eduardo Serrano <eduardo.serrano@lexisnexis.co.uk>
2023-10-23 11:02:01 +0100

    note on construct signatures

--- code-demos/communication-event-bus-ng16/shell-ng16/src/app/event-bus.ts
+++ code-demos/communication-event-bus-ng16/shell-ng16/src/app/event-bus.ts
@@ -19,6 +19,21 @@ export class EventBus {
     this._subject.next(event);
   }
 
+  // TODO add this info to the README as well
+  // The input param `t: new (...args: any[]) => T` is a construct signature
+  // It allows the following syntax:
+  // eventBus.GetEvent(PaymentEvent).Subscribe(...)
+  //
+  // It restricts the input parameter to something that is of a certain type, in the
+  // example given of the type PaymentEvent, and it must have a constructor with zero
+  // or more arguments of any type. Meaning PaymentEvent can't be a type or interface,
+  // it must be a class
+  //
+  // For more info see:
+  //
+  // - Typescript docs on Construct Signatures -> https://www.typescriptlang.org/docs/handbook/2/functions.html#construct-signatures
+  // - FAQs on microsoft/TypeScript repo: "Why can't I write typeof T, new T, or instanceof T in my generic function?"" -> https://github.com/microsoft/TypeScript/wiki/FAQ#why-cant-i-write-typeof-t-new-t-or-instanceof-t-in-my-generic-function
+  // - Issue on microsoft/TypeScript repo: "Suggestion: typeof of generic type" -> https://github.com/microsoft/TypeScript/issues/204
   public getEvent$<T>(t: new (...args: any[]) => T): Observable<T> {
     return this._subject.pipe(
       filter((e): e is T => e instanceof t)
