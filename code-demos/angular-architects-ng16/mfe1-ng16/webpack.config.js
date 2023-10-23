fc7736e566c999cd0f90bee23ed7641c3a720f3b
Eduardo Serrano <eduardo.serrano@lexisnexis.co.uk>
2023-10-23 12:45:40 +0100

    angular-architects-ng16: add console logs to webpack config

--- code-demos/angular-architects-ng16/mfe1-ng16/webpack.config.js
+++ code-demos/angular-architects-ng16/mfe1-ng16/webpack.config.js
@@ -3,7 +3,7 @@ const {
   withModuleFederationPlugin,
 } = require("@angular-architects/module-federation/webpack");
 
-module.exports = withModuleFederationPlugin({
+const webpackConfig = withModuleFederationPlugin({
   name: "mfe1-ng16",
   filename: "remoteEntry.js", // this doesn't need to be set, if not specified it defaults to remoteEntry.js. Setting it here just for demo purposes.
   exposes: {
@@ -17,3 +17,21 @@ module.exports = withModuleFederationPlugin({
     }),
   },
 });
+module.exports = webpackConfig;
+
+// The lines below are NOT needed. They are just here to make it easier to understand
+// what webpack configuration values the `withModuleFederationPlugin` function is
+// setting up.
+//
+// These logs will show up when you run `npm start` for the mfe1-ng16 app
+console.log("\n\n==========WEBPACK ROOT LEVEL CONFIG==========")
+console.log(webpackConfig);
+console.log("==========WEBPACK ROOT LEVEL CONFIG==========")
+
+console.log("\n\n==========WEBPACK ModuleFederationPlugin CONFIG==========")
+console.log(webpackConfig.plugins[0]);
+console.log("==========WEBPACK ModuleFederationPlugin CONFIG==========")
+
+console.log("\n\n==========WEBPACK FULL CONFIG==========")
+console.log(JSON.stringify(webpackConfig, null, 2));
+console.log("==========WEBPACK FULL CONFIG==========\n\n")
--- code-demos/angular-architects-ng16/shell-ng16/webpack.config.js
+++ code-demos/angular-architects-ng16/shell-ng16/webpack.config.js
@@ -3,7 +3,7 @@ const {
   withModuleFederationPlugin,
 } = require("@angular-architects/module-federation/webpack");
 
-module.exports = withModuleFederationPlugin({
+const webpackConfig = withModuleFederationPlugin({
   remotes: {
     mfe1: "http://localhost:4201/remoteEntry.js",
   },
@@ -15,3 +15,22 @@ module.exports = withModuleFederationPlugin({
     }),
   },
 });
+module.exports = webpackConfig;
+
+// The lines below are NOT needed. They are just here to make it easier to understand
+// what webpack configuration values the `withModuleFederationPlugin` function is
+// setting up.
+//
+// These logs will show up when you run `npm start` for the shell-ng16 app
+console.log("\n\n==========WEBPACK ROOT LEVEL CONFIG==========")
+console.log(webpackConfig);
+console.log("==========WEBPACK ROOT LEVEL CONFIG==========")
+
+console.log("\n\n==========WEBPACK ModuleFederationPlugin CONFIG==========")
+console.log(webpackConfig.plugins[0]);
+console.log("==========WEBPACK ModuleFederationPlugin CONFIG==========")
+
+console.log("\n\n==========WEBPACK FULL CONFIG==========")
+console.log(JSON.stringify(webpackConfig, null, 2));
+console.log("==========WEBPACK FULL CONFIG==========\n\n")
+
